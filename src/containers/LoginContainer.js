7a72a099674adb54e81efe9334e49f076edcee28
Giljun Bae <bgj0127@gmail.com>
2021-03-30 15:16:24 +0900

    [FEAT]<Auth> Container

---
+++ src/containers/LoginContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Login from '../components/auth/login/Login';
+
+const LoginContainer = () => {
+  return (
+    <div>
+      <Login
+
+      />
+    </div>
+  );
+};
+
+export default LoginContainer;
---
+++ src/containers/RegisterContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Register from '../components/auth/register/Register';
+
+const RegisterContainer = () => {
+  return (
+    <div>
+      <Register
+      />
+    </div>
+  );
+};
+
+export default RegisterContainer;
