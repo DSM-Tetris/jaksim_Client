ddc2ae098a6c5dd51cc3166d35b114992c8e255d
Giljun Bae <bgj0127@gmail.com>
2021-04-03 16:41:02 +0900

    [FEAT]<GraphQL> graphQL 대비

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { hot } from 'react-hot-loader';
 import { BrowserRouter } from 'react-router-dom';
 import Routing from './routers/Routing';
+// import GraphQL from './components/GraphQL';
+import GlobalStyle from './globalStyle';
 
 const App = () => {
 
   return (
     <BrowserRouter>
-      {/* <GlobalStyle /> */}
+      <GlobalStyle />
       <Routing />
+      {/* <GraphQL /> */}
     </BrowserRouter>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import GlobalStyle from './globalStyle';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: "https://countries.trevorblades.com",
+})
 
 ReactDOM.render(
-  <>
+  <ApolloProvider client={client}>
     <GlobalStyle/>
     <App />
-  </>, 
+  </ApolloProvider>,
   document.getElementById("root")
 )
