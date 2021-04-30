297afedf8c280b60f939f4a1760f0e94f1793e78
Giljun Bae <bgj0127@gmail.com>
2021-04-30 15:15:34 +0900

    [FEAT]<Main> 메인페이지 퍼블리싱 시작

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './style';
 import {Background} from '../shareStyle';
 import Header from '../header/Header';
 
 const Main = () => {
+  const [isPost, setIsPost] = useState(true);
+
   return (
     <div>
       <Background />
       <S.Container>
         <Header />
+        <S.TopBox>
+          <S.TodayTag>오늘의 태그는 <S.TagText>#플라스틱</S.TagText> 입니다</S.TodayTag>
+          <S.TopInner>
+            <S.TextBox>
+              <S.Write>{isPost ? "NOW" : "NOT"}</S.Write>
+              <S.Green>GREEN</S.Green>
+            </S.TextBox>
+            <S.BatteryBox>
+              <S.BatteryOutter>
+                <S.BatteryInner>
+                  <S.Test />
+                  <S.Tet />
+                </S.BatteryInner>
+              </S.BatteryOutter>
+            </S.BatteryBox>
+          </S.TopInner>
+          <S.PostBtn>전체 글 보기</S.PostBtn>
+        </S.TopBox>
       </S.Container>
     </div>
   );
--- src/components/main/style.js
+++ src/components/main/style.js
@@ -1,4 +1,23 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const wave = keyframes`
+  0%{
+    background-position-x: 0px;
+  }
+  100%{
+    background-position-x: 1000px;
+  }
+`;
+
+const wave2 = keyframes`
+  0%{
+    background-position-x: 400px;
+  }
+  100%{
+    background-position-x: 1400px;
+  }
+`;
+
 
 export const Container = styled.div`
   display: flex;
@@ -6,3 +25,126 @@ export const Container = styled.div`
   width: 100%;
   height: 200vh;
 `
+
+export const TopBox = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 90vh;
+  margin-top: 100px;
+  /* background-color: wheat; */
+`
+
+export const TodayTag = styled.div`
+  font-size: 50px;
+`
+
+export const TagText = styled.span`
+  font-size: 50px;
+  color: #4A7A62;
+  cursor: pointer;
+  transition: opacity 0.2s ease-in-out;
+
+  :hover{
+    opacity: 0.5;
+  }
+`
+
+export const TopInner = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 80%;
+  height: 80%;
+  /* background-color: wheat; */
+`
+
+export const TextBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 100px;
+  width: 48%;
+  height: 100%;
+  box-sizing: border-box;
+  /* background-color: darkgreen; */
+`
+
+export const Write = styled.span`
+  font-size: 130px;
+  
+`
+
+export const Green = styled.span`
+  font-size: 130px;
+  color: #4A7A62;
+`
+
+export const BatteryBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 48%;
+  height: 100%;
+  box-sizing: border-box;
+  /* background-color: firebrick; */
+`
+
+export const BatteryOutter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 440px;
+  height: 440px;
+  border-radius: 50%;
+  background-color: rgba(255,255,255,0.7);
+`
+
+export const BatteryInner = styled.div`
+  position: relative;
+  width: 400px;
+  height: 400px;
+  border-radius: 50%;
+  background-color: white;
+  clip-path: circle();
+`
+
+export const Test = styled.div`
+  position: absolute;
+  left: 0;
+  /* top: -200px; */
+  width: 100%;
+  height: 700px;
+  background-size: 500px;
+  background-position-x: 0;
+  background-image: url("/src/assets/images/asdf.png"); 
+  /* transition: all 5s ease-in; */
+  /* opacity: 0.8;  */
+  animation: ${wave} 20s linear infinite;
+`
+
+export const Tet = styled.img`
+  position: absolute;
+  left: 0;
+  /* top: -200px; */
+  width: 100%;
+  height: 700px;
+  background-size: 500px;
+  background-position-x: 400px;
+  background-image: url("/src/assets/images/asdf.png"); 
+  animation: ${wave2} 20s linear infinite;
+  opacity: 0.6;
+  z-index: 100;
+`
+
+export const PostBtn = styled.button`
+  width: 35%;
+  height: 70px;
+  background-color: #DEFADA;
+  border: none;
+  outline: none;
+  font-size: 35px;
+  font-family: 'paybooc-Medium';
+`
+
