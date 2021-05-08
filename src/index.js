54a4a253eb1d15db3f3218600235a272797f5f56
Giljun Bae <bgj0127@gmail.com>
2021-05-08 17:05:45 +0900

    [FEAT]<index> apollo client

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import GlobalStyle from './globalStyle';
-import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
