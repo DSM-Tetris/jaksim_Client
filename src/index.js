4d9144a5d2568c209437e7a331e79d26ff543f02
Giljun Bae <bgj0127@gmail.com>
2021-05-13 10:04:36 +0900

    [FEAT]<index> graphQL client 수정

--- src/index.js
+++ src/index.js
@@ -4,10 +4,29 @@ import App from './App'
 import GlobalStyle from './globalStyle';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from '@apollo/client/link/context'
+import { createHttpLink } from '@apollo/client';
+import { getToken } from './lib/token/GetToken';
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: "http://10.156.146.199:3000/",
 })
+
+const authLink = setContext((_, {headers}) => {
+  const token = getToken('token');
+  return{ 
+    headers: {
+    ...headers,
+    authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
   <ApolloProvider client={client}>
