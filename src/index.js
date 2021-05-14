013173232485a874c57a12507fa8d2149e09aaef
Giljun Bae <bgj0127@gmail.com>
2021-05-14 15:10:07 +0900

    [FEAT]<FragmentTypes> fragmentType

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import App from './App'
 import GlobalStyle from './globalStyle';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import { InMemoryCache,IntrospectionFragmentMatcher } from 'apollo-cache-inmemory';
 import { setContext } from '@apollo/client/link/context'
 import { createHttpLink } from '@apollo/client';
 import { getToken } from './lib/token/GetToken';
+import introspectionQueryResultData from './fragmentTypes.json';
 
 const httpLink = createHttpLink({
   uri: "http://10.156.146.199:3000/",
@@ -23,9 +24,15 @@ const authLink = setContext((_, {headers}) => {
   }
 });
 
+const fragmentMatcher = new IntrospectionFragmentMatcher({
+  introspectionQueryResultData
+});
+
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
-  cache: new InMemoryCache()
+  cache: new InMemoryCache({
+    fragmentMatcher,
+  })
 });
 
 ReactDOM.render(
