6f177367919e57dbe12ec80e4f610a0234dce920
Giljun Bae <bgj0127@gmail.com>
2021-05-25 18:55:16 +0900

    [FIX]<PostContainer> 헤더 토큰 넣어서 request 보내기

--- src/GraphQL/Queries.js
+++ src/GraphQL/Queries.js
@@ -3,27 +3,17 @@ import { gql } from 'apollo-boost';
 export const GET_POSTS = gql`
   query GetPosts($page: Int!, $categoryId: Int) {
     getPosts(data: {page: $page, categoryId: $categoryId}) {
+      __typename
       ... on GetPosts {
-        __typename
-        posts {
-          title
-          contentPreview
-          image
-          tags {
-            tagName
-          }
-        }
-      }
-      ... on Unauthorized {
-        __typename  
         message
       }
       ... on BadRequest {
-        __typename
+        message
+      }
+      ... on Unauthorized {
         message
       }
       ... on NotFoundAnyPost {
-        __typename
         message
       }
     }
--- src/containers/PostContainer.js
+++ src/containers/PostContainer.js
@@ -1,32 +1,28 @@
-import React, { useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
 import {useQuery} from '@apollo/client';
 import Write from '../components/write/Write';
 import {GET_POSTS} from '../GraphQL/Queries';
 
 const PostContainer = () => {
-
+  const [a,setA] = useState({});
   const token = localStorage.getItem("token");
 
-    let page = 1;
-    function hello() {
-      const {loading, error, data} = useQuery(GET_POSTS, {
-        variables: {page},
-        context: {
-          headers: {
-            authorization: token ? `Bearer ${token}` : ""
-          },
-        },
-      });
+  const {loading, error, data} = useQuery(GET_POSTS, {
+    variables: {page: 1},
+    context: {
+      headers: {
+        authorization: token ? `Bearer ${token}` : ""
+      },
+    },
+  });
 
-      if (loading) return console.log("loading...");
-      if (error) return console.log("error!");
-      
-      return (
-        console.log(data)
-      );
+  useEffect(()=>{
+    if(data && data.getPosts) {
+      setA(data.getPosts.message);
     }
+  },[data]);
 
-    hello();
+  console.log(a);
 
   return (
     <Write />
