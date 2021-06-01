8726fc26a4ef3d623167a65d4b462538974a7b43
Giljun Bae <bgj0127@gmail.com>
2021-06-01 20:03:34 +0900

    [FIX]<Header> 리다이렉트

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import * as S from './style';
 
 const Header = () => {
+  const history = useHistory();
+
+  const logout = () => {
+    alert("로그아웃에 성공하셨습니다.");
+    history.push("/login");
+    localStorage.removeItem("token");
+    localStorage.removeItem("refresh");
+  }
+
   return (
-    <S.Logout >로그아웃</S.Logout>
+    <S.Logout onClick={logout}>로그아웃</S.Logout>
   );
 };
 
